2525d3e3ab76cf83ef09a353d3b6169e46d62511
Megale <megale@gmail.com>
2018-06-09 19:32:54 -0300

    Cria tabela veículos com todos os campos

--- sqls/CriaTabelas.sql
+++ sqls/CriaTabelas.sql
@@ -1,3 +1,8 @@
-select nome, cpf, endereco, telefone, sexo, dataDeNascimento from CLIENTES order by nome;
 insert into CLIENTES (cpf,nome,endereco,telefone,sexo,dataDeNascimento) values ('06722' , 'megale' , 'viamao869' , '33715002' , 'm' , '1984-12-04');
+insert into VEICULOS (placa,nome,modelo,marca,anoDeFabricacao,anoDeVenda) values ('hmp-0774' , 'Civic' , 'ELX' , 'Honda' , '1998' , '2001');
+insert into VEICULOS (placa,nome,modelo,marca,anoDeFabricacao,anoDeVenda) values ('GZN-2978' , 'Grand Vitara' , 'Auto' , 'Suzuki' , '2001' , '2014');
+
+select placa,nome,modelo,marca,anoDeFabricacao,anoDeVenda from VEICULOS order by placa;
+select nome, cpf, endereco, telefone, sexo, dataDeNascimento from CLIENTES order by nome;
+
 
